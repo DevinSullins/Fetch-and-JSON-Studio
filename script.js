e071bfb4dc789d36966c97f24ef334b2c0804144
Devin <devinsullins@gmail.com>
2020-12-07 19:22:02 -0600

    added javascript loop to display Astronauts from fetch request

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( (res) => {
+    res.json().then((json) => {
+        let container = document.getElementById("container")
+        const astronautArr = json
+        for (let i = 0; i < astronautArr.length; i++){
+        const currentAstronaut = astronautArr[i]
+        let skillList = currentAstronaut.skills.join(', ')
+        // for(skill in json[i].skills){
+
+        // }
+        container.innerHTML += `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${astronautArr[i].firstName} ${astronautArr[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronautArr[i].hoursInSpace}</li>
+                        <li>Active: ${astronautArr[i].active}</li>
+                        <li>Skills: ${skillList}</li>
+                    </ul>
+            </div>
+        <img class="avatar" src="${astronautArr[i].picture}">
+        </div>
+        `
+        console.log(astronautArr[i])
+
+        }
+    })
+});
